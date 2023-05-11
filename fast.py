ce7e8d82b0366b12cd6bf5cb1fbcb8a9082679e8
Kamel BEN ABDELKADER <kamel.benabdelkader69@gmail.com>
2023-05-11 23:53:13 +0200

    test method crud get et post

--- fast.py
+++ fast.py
@@ -1,7 +1,7 @@
 # 1. Library imports
 import uvicorn
 from typing import List
-from models import Test
+from models import Test,  TableBdd
 from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 import pymysql
@@ -32,8 +32,10 @@ conn = pymysql.connect(
     password=db_password,
     database=db_name
 )
-def fetch_items(table_name: str, limit: int = None) -> List[Test]:
-    # Perform operations on the database
+
+#----------------- Function a placer dans un autre fichier et a refacto-----------------------------#
+def fetch_items(table_name: str, limit: int = None) -> List[TableBdd]:
+    # Effectuer des opérations sur la base de données
     with conn.cursor() as cursor:
         query = f"SELECT * FROM {table_name}"
         if limit is not None:
@@ -41,20 +43,102 @@ def fetch_items(table_name: str, limit: int = None) -> List[Test]:
         cursor.execute(query)
         results = cursor.fetchall()
 
-    # Convert the results to a list of Test objects
-    items = [Test(**row) for row in results]
+    items = []
+    for row in results:
+        item = TableBdd(
+            YEAR=row[0],
+            QUARTER=row[1],
+            MONTH=row[2],
+            DAY_OF_MONTH=row[3],
+            DAY_OF_WEEK=row[4],
+            FL_DATE=row[5],
+            UNIQUE_CARRIER=row[6],
+            AIRLINE_ID=row[7],
+            CARRIER=row[8],
+            TAIL_NUM=row[9],
+            FL_NUM=row[10],
+            ORIGIN_AIRPORT_ID=row[11],
+            ORIGIN_AIRPORT_SEQ_ID=row[12],
+            ORIGIN_CITY_MARKET_ID=row[13],
+            ORIGIN=row[14],
+            ORIGIN_CITY_NAME=row[15],
+            ORIGIN_STATE_ABR=row[16],
+            ORIGIN_STATE_FIPS=row[17],
+            ORIGIN_STATE_NM=row[18],
+            ORIGIN_WAC=row[19],
+            DEST_AIRPORT_ID=row[20],
+            DEST_AIRPORT_SEQ_ID=row[21],
+            DEST_CITY_MARKET_ID=row[22],
+            DEST=row[23],
+            DEST_CITY_NAME=row[24],
+            DEST_STATE_ABR=row[25],
+            DEST_STATE_FIPS=row[26],
+            DEST_STATE_NM=row[27],
+            DEST_WAC=row[28],
+            CRS_DEP_TIME=row[29],
+            DEP_TIME=row[30],
+            DEP_DELAY=row[31],
+            DEP_DELAY_NEW=row[32],
+            DEP_DEL15=row[33],
+            DEP_DELAY_GROUP=row[34],
+            DEP_TIME_BLK=row[35],
+            TAXI_OUT=row[36],
+            WHEELS_OFF=row[37],
+            WHEELS_ON=row[38],
+            TAXI_IN=row[39],
+            CRS_ARR_TIME=row[40],
+            ARR_TIME=row[41],
+            ARR_DELAY=row[42],
+            ARR_DELAY_NEW=row[43],
+            ARR_DEL15=row[44],
+            ARR_DELAY_GROUP=row[45],
+            ARR_TIME_BLK=row[46],
+            CANCELLED=row[47],
+            CANCELLATION_CODE=row[48],
+            DIVERTED=row[49],
+            CRS_ELAPSED_TIME=row[50],
+            ACTUAL_ELAPSED_TIME=row[51],
+            AIR_TIME=row[52],
+            FLIGHTS=row[53],
+            DISTANCE=row[54],
+            DISTANCE_GROUP=row[55],
+            CARRIER_DELAY=row[56],
+            WEATHER_DELAY=row[57],
+            NAS_DELAY=row[58],
+            SECURITY_DELAY=row[59],
+            LATE_AIRCRAFT_DELAY=row[60],
+            FIRST_DEP_TIME=row[61],
+            TOTAL_ADD_GTIME=row[62],
+            LONGEST_ADD_GTIME=row[63],
+            Column65=row[64]
+        )
+        items.append(item)
+
+    # #Pour refacto remplacer par
+    # items = []
+    # for row in results:
+    #     item = TableBdd(*row[:65])
+    #     items.append(item)
+
+
+    # Retourner les résultats de l'API
     return items
 
 
-@app.get("/items/{table_name}")
-async def get_items(table_name: str, limit: int = None) -> List[Test]:
-    if table_name not in ["test", "janvier"]:
+
+
+#----------------- Définir les routes de l'API-----------------------------#
+
+#Prendre les mois en vue de faire des KBI ou des graphes
+@app.get("/mois/{table_name}")
+async def get_items(table_name: str, limit: int = None) -> List[TableBdd]:
+    if table_name not in ["janvier","fevrier","mars","avril", "mai","juin","juillet","aout","septembre","octobre","novembre","decembre"]:
         raise HTTPException(status_code=404, detail="Table not found")
 
-    items = fetch_items(table_name, limit)
+    items = fetch_items(table_name, limit=5)
     return items
 
-# Définir les routes de l'API
+# Petit test de co avec method get sur le home /
 @app.get("/")
 async def get_items() -> List[Test]:
     # Effectuer des opérations sur la base de données
@@ -62,7 +146,7 @@ async def get_items() -> List[Test]:
         cursor.execute("SELECT * FROM test")
         results = cursor.fetchall()
 
-    # Convertir les résultats en une liste d'objets Test
+    # Convertir les résultats en une liste d'objets Test a refacto par la suite
     items = []
     for row in results:
         test_dict = {
@@ -114,6 +198,10 @@ async def create_item(item: Test):
 
     # return new_item
 
+## faire les autres method CRUD reste update, delete put
+##Faire script pour le model
+##Penser à reduite les appel a la bdd
+##refacto ....
 
 # # 4. Run the API with uvicorn
 # #    Will run on http://127.0.0.1:8000
